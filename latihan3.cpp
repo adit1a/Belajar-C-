e2615bbe29b50cd7ab65739dc4ff2c2f4e01f59e
adit1a <ardimuh50@gmail.com>
2026-05-02 07:43:45 +0800

    mempelajari fungsi

--- latihan3.cpp
+++ latihan3.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 using namespace std;
+
+void cetakGaris(){
+    cout<<"=============================="<<"\n";
+}
+bool stokBarang(int jumlahBarang){
+  int stokGudang = 5;
+  if(jumlahBarang <= stokGudang){
+    cout<<"stok barang tersedia"<<endl;
+    return true;
+  }else{
+    cout<<"stok barang habis"<<endl;
+    return false;
+  }
+}
 int main(){
     double total =0;
     int jumlahBarang;
+    cetakGaris();
     cout<<"===kasir pro dengan struk==="<<"\n";
+    cetakGaris();
+    
     cout<<"jumlah barang yang kamu beli: ";
     cin>>jumlahBarang;
+    if(stokBarang(jumlahBarang) == false){
+        return 0;
+    }
 
     double daftarHarga[jumlahBarang];
     string daftarNamaBarang[jumlahBarang];
@@ -18,13 +38,14 @@ int main(){
         cin>>daftarHarga[i];
         total += daftarHarga[i];
     }
-
+    
     cout<<"\n===struk belanja==="<<endl;
     for(int i=0; i<jumlahBarang; i++){
         cout<<i+1<<". " <<daftarNamaBarang[i]<<"\t: - Rp"<<daftarHarga[i]<<endl;
     }
     cout<<"------------------------------"<<endl;
     cout<<"subtotal : "<<total<<endl;
+    
     if(total > 100000){
         double diskon = total * 0.1;
         cout<<"diskon 10% : Rp"<<diskon<<endl;
