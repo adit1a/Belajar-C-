d065cb09faeb52d38c2d4807b1ab92b9110ad2f7
adit1a <ardimuh50@gmail.com>
2026-05-08 21:54:03 +0800

    refactor

--- tugas2_gameSederhana.cpp
+++ tugas2_gameSederhana.cpp
@@ -12,10 +12,12 @@ struct game
     string nama;
     int senjata;
     int musuh;
-    int jalan;
+    int serang;
 };
 struct karakter{
     string nama;
+    int attack;
+    int hp;
 
 };
 
@@ -52,6 +54,7 @@ int main(){
     srand(time(0));
     game pemain1;
     karakter npc1;
+    karakter npc2;
     npc1.nama = "Gorgon";
     Ketik( npc1.nama + " :halo pemain selamat datang di penghancuran hahah,", 10);
     
@@ -61,42 +64,45 @@ int main(){
     getline(cin, pemain1.nama);
     Ketik( npc1.nama +" :ok jadi nama kamu adalah " + pemain1.nama + " hhahah nama yang lucu", 10);
     Ketik(npc1.nama + " :sekarang " + pemain1.nama + " kamu harus memilih senjata agar bertahan hidup", 10);
-    cout<<npc1.nama +" :di depan kamu ada 5 kotak silahkan pilih"<<endl;
     cout<<"silahkan pilih :";
     int pilihan;
-    cin>>pilihan;
-    if( pilihan < 1 || pilihan > 3){
+    do{
+        cout<<npc1.nama +" :di depan kamu ada 3 kotak silahkan pilih"<<endl;
+        ketik(npc1.nama+" silahkan pilih "+ pemain1.nama +" percayalah hatimu", 20);
+        cin>>pilihan;
+        if( pilihan < 1 || pilihan > 3){
         Ketik("pilihan kamu tidak ada bodoh dan mati", 10);
-        return 0;
     }
+    } while (pilihan  < 1  || pilihan > 3);
+   
     Ketik("Kamu membuka kotak nomor " + to_string(pilihan) + "...", 50);
     pemain1.senjata=rand() % 3+1;
     string senjataPemain=cekSenjata(pemain1.senjata);
     Ketik("Ternyata isinya adalah: " + senjataPemain + "!", 20);
     pemain1.musuh =rand () % 12;
-    if(senjataPemain == "salah"){
-        Ketik("kamu salah pilih dan dibunuh game over bunng", 150);
-        return 0;
-    }
-    else if(senjataPemain == "pisau"  && pemain1.musuh <= 4){
+    if(senjataPemain == "pisau"  && pemain1.musuh <= 4){
        cout<<"kamu menang ya""\n";
        Ketik(npc1.nama +" :hahah bagus sekarang maju kedepan ", 20);
        ketik(npc1.nama +" :awas ada percabangan kanan atau kiri : ", 20);
        int pilihan;
        cin>>pilihan;
-
        if (pilihan == 1 )
        {
-            ketik("kamu memilih " + to_string(pilihan) + "kamu keren",20);
+            Ketik("kamu memilih " + to_string(pilihan) + " kamu keren",20);
+            Ketik(npc1.nama+ " :kamu lewat jalan ini awas hati hati...", 20);
+            if (senjataPemain == "pisau" && pemain1.musuh < 1)
+            {
+                Ketik(npc1.nama+ " :kamu menghadapi musuh serang ", 20);
+                Ketik(npc1.nama + " :kamu harus membunuh musuh" ,20);
 
+            }
        }
-       else{
-        Ketik("kamu memilih jalan salah", 20);
+       else if(pilihan == 2){
+        Ketik("kamu memilih jalan kiri ", 20);
        }
-       
-    }else if(senjataPemain == "pisau"  && pemain1.musuh > 5){
-        cout<<"kamu mati \n";
-        return 0;
+        if (senjataPemain == "pisau" && pemain1.musuh > 5) {
+            cout<<"kamu mati \n";
+        }
     }
     else if(senjataPemain == "pistol" && pemain1.musuh <= 6){
         cout<<"kamu menangg""\n";
@@ -107,11 +113,16 @@ int main(){
 
        if (pilihan == 1 )
        {
-            ketik("kamu memilih " + to_string(pilihan) + " kamu keren",20);
-
+            Ketik("kamu memilih " + to_string(pilihan) + " kamu keren",20);
+            Ketik(npc1.nama+ " :kamu lewat jalan ini awas hati hati...", 20);
+            if (senjataPemain == "pistol" && pemain1.musuh <= 2);
        }
-       else{
-        Ketik("kamu memilih jalan salah", 20);
+       else if(pilihan == 2){
+        Ketik("kamu memilih " + to_string(pilihan) + " kamu keren",20);
+        Ketik(npc1.nama+ " :kamu lewat jalan ini awas hati hati...", 20);
+            if (senjataPemain == "pistol" && pemain1.musuh >= 3){
+                Ketik(npc1.nama + " :kamu memilih ini dan mati", 20);
+            }
        }
 
     }
@@ -125,10 +136,15 @@ int main(){
        ketik(npc1.nama +" :awas ada percabangan kanan atau kiri ", 20);
        int pilihan;
        cin>>pilihan;
-
        if (pilihan == 1 )
        {
-            ketik("kamu memilih " + to_string(pilihan) + " kamu keren",20);
+            Ketik("kamu memilih " + to_string(pilihan) + " kamu keren",20);
+            Ketik(npc1.nama+ " :kamu lewat jalan ini awas hati hati...", 20);
+            if (senjataPemain == "Desert Eagle" && pemain1.musuh < 6){
+                Ketik(npc1.nama +" :kamu membantai ya di jalan ini", 20);
+            }else{
+                Ketik(npc1.nama +" kamu sangat pandai memilih", 20);
+            }
 
        }
        else{
@@ -141,6 +157,7 @@ int main(){
     }else{
         cout<< "kamu kalah "<<endl;
     }
+    Ketik("selamat telah menamatkan game ini " + pemain1.nama, 20);
     return 0;
 
 }
