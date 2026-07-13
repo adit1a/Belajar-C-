247febdfa7f0aba1ba647ad65ed5c733efbea7c9
adit1a <ardimuh50@gmail.com>
2026-07-14 00:20:26 +0800

    menambah logika reset koin

--- latihan1.2.cpp
+++ latihan1.2.cpp
@@ -1,18 +1,25 @@
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 #include "raylib.h"
 
 using namespace std;
 
-void layar(){
+// void layar(){
+//     const int lebarLayar = 800;
+//     const int tinggiLayar = 400;
+//     InitWindow(lebarLayar, tinggiLayar, " Belajar Logika koin");
+
+// }
+
+int main(){
+    // layar();
     const int lebarLayar = 800;
     const int tinggiLayar = 400;
     InitWindow(lebarLayar, tinggiLayar, " Belajar Logika koin");
-
-}
-
-int main(){
-    layar();
     SetTargetFPS(60);
+
+    srand(time(0)); 
 
     // karakter
     Rectangle player ={100, 250, 40, 40};
@@ -21,9 +28,13 @@ int main(){
 
     // wadah
     vector<Rectangle> daftarKoin;
-    daftarKoin.push_back({300, 260, 20, 20});
-    daftarKoin.push_back({500, 200, 20, 20});
-    daftarKoin.push_back({600, 400, 20, 20});
+    for(int i=0; i<3; i++){
+        float acakX = rand() % (lebarLayar - 40)+20;
+        float acakY = rand() % (tinggiLayar - 100)+60;
+        daftarKoin.push_back({acakX, acakY, 20, 20});
+    }
+    Rectangle tombolReset = { 660, 20, 120, 40 };
+    Color warnaTombol = LIGHTGRAY;
 
     while(!WindowShouldClose()){
         // pergerakan player
@@ -31,6 +42,25 @@ int main(){
         if(IsKeyDown(KEY_A)) player.x -= speed;
         if(IsKeyDown(KEY_W)) player.y -= speed;
         if(IsKeyDown(KEY_S)) player.y += speed;
+
+        // ambil posisi mouse
+        Vector2 posisiMouse = GetMousePosition();
+
+        // cek apakah mouse hover di tombol reset
+        if(CheckCollisionPointRec(posisiMouse, tombolReset)){
+            warnaTombol = GRAY;
+            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+                totalKoin = 0;
+                daftarKoin.clear();
+                for(int i=0; i<3; i++){
+                    float acakX = rand() % (lebarLayar - 40)+20;
+                    float acakY = rand() % (tinggiLayar - 100)+60;
+                    daftarKoin.push_back({acakX, acakY, 20, 20});
+                }
+            }
+        } else {
+            warnaTombol = LIGHTGRAY;
+        }
 
         // logika memakan koin
         auto it = daftarKoin.begin();
@@ -49,8 +79,12 @@ int main(){
                 DrawRectangleRec(koin, GOLD);
             }
             DrawRectangleRec(player, BLUE);
+            DrawRectangleRec(tombolReset, warnaTombol);
+            DrawRectangleLinesEx(tombolReset, 2, BLACK);
+            DrawText("RESET", tombolReset.x + 30, tombolReset.y + 12, 16, BLACK);
             DrawText(TextFormat("koin dikoleksi : %d", totalKoin), 20,20,22, BLACK);
             DrawText("dekati koin emas untuk di koleksi", 20, 50, 20, GRAY);
+            
         EndDrawing();
     }
 CloseWindow();
